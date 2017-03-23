796b89e953609fbc2064dab3e3ce24c29d24d65b
James Harris <james.harris@icecave.com.au>
2017-03-23 14:21:19 +1000

    Conditionally set the grit function only if the grit binary is found.

--- etc/grit.bash
+++ etc/grit.bash
@@ -1,36 +1,40 @@
 # Store the location of the real grit binary.
 GRIT_BIN=$(which grit)
 
-# Make a bash function named grit so that we can execute our directory changes
-# in the current shell.
-grit() {
-    case $1 in
-        # Intercept commands that print directories to STDOUT.
-        clone|cd)
-            local dir=$($GRIT_BIN "$@")
-            if [ -d "$dir" ]; then
-                cd "$dir"
-                return 0
-            fi
+if [ -f "$GRIT_BIN" ]; then
 
-            return 1
-        ;;
-        # Pass through other commands unchanged.
-        *)
-            $GRIT_BIN "$@"
-            return $?
-        ;;
-    esac
-}
+    # Make a bash function named grit so that we can execute our directory changes
+    # in the current shell.
+    grit() {
+        case $1 in
+            # Intercept commands that print directories to STDOUT.
+            clone|cd)
+                local dir=$($GRIT_BIN "$@")
+                if [ -d "$dir" ]; then
+                    cd "$dir"
+                    return 0
+                fi
 
-# Setup autocompletion using the real binary.
-_grit_bash_autocomplete() {
-    local cur opts base
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    opts=$($GRIT_BIN ${COMP_WORDS[@]:1:$COMP_CWORD} --generate-bash-completion)
-    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-    return 0
-}
+                return 1
+            ;;
+            # Pass through other commands unchanged.
+            *)
+                $GRIT_BIN "$@"
+                return $?
+            ;;
+        esac
+    }
 
-complete -F _grit_bash_autocomplete grit
+    # Setup autocompletion using the real binary.
+    _grit_bash_autocomplete() {
+        local cur opts base
+        COMPREPLY=()
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        opts=$($GRIT_BIN ${COMP_WORDS[@]:1:$COMP_CWORD} --generate-bash-completion)
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    }
+
+    complete -F _grit_bash_autocomplete grit
+
+fi
